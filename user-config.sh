4443916a999bfb33536dd271db1fab1294f3500f
Sguazt <sguazt@scramble>
2010-06-07 11:14:26 +0200

    Minor updates

--- user-config.sh
+++ user-config.sh
@@ -11,8 +11,8 @@ USR_CFLAGS=
 # Set here you C++ compiler flags
 #USR_CXXFLAGS=
 #USR_CXXFLAGS=-I../dcsxx/src
-USR_CXXFLAGS="-I../dcsxx/src -I$HOME/tmp/cvs-prj/boost-trunk"
-#USR_CXXFLAGS="-I../dcsxx/src -I$HOME/Sys/usr/include/boost"
+USR_CXXFLAGS="-I../dcsxx/src -I../boost-ublas -I$HOME/tmp/cvs-prj/boost-trunk"
+#USR_CXXFLAGS="-I../dcsxx/src -I../boost-ublas -I$HOME/Sys/usr/include/boost"
 # Set here you C/C++ linker flags
 USR_LDFLAGS=
 
