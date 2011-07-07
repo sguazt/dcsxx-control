e17291fa11c09b5ccc2eecaf353fcf3a92ed0977
Sguazt <sguazt@scramble>
2011-07-07 10:00:29 +0200

    (new|experimental) Added controllable decomposition class.

--- src/dcs/control/analysis/controllability.hpp
+++ src/dcs/control/analysis/controllability.hpp
@@ -124,9 +124,15 @@
 #include <boost/numeric/ublas/matrix_expression.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/traits.hpp>
+#include <boost/numeric/ublasx/operation/cat.hpp>
+#include <boost/numeric/ublasx/operation/eps.hpp>
 #include <boost/numeric/ublasx/operation/num_columns.hpp>
 #include <boost/numeric/ublasx/operation/num_rows.hpp>
 #include <boost/numeric/ublasx/operation/rank.hpp>
+#include <boost/numeric/ublasx/operation/rot90.hpp>
+#include <boost/numeric/ublasx/operation/sum.hpp>
+#include <boost/numeric/ublasx/operation/svd.hpp>
+#include <limits>
 
 
 namespace dcs { namespace control {
@@ -345,6 +351,319 @@ bool is_output_controllable(::boost::numeric::ublas::matrix_expression<AMatrixT>
 			==
 			::boost::numeric::ublasx::num_rows(C);
 }
+
+
+template <typename ValueT>
+class controllable_decomposition
+{
+	public: typedef ValueT value_type;
+	public: typedef ::std::size_t size_type;
+	public: typedef ::boost::numeric::ublas::matrix<value_type> matrix_type;
+	public: typedef ::boost::numeric::ublas::vector<size_type> vector_type;
+
+
+	public: controllable_decomposition()
+	{
+	}
+
+
+	public: template <
+				typename AMatrixT,
+				typename BMatrixT
+			>
+		controllable_decomposition(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+								   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B)
+	{
+		decompose(A, B);
+	}
+
+
+	public: template <
+				typename AMatrixT,
+				typename BMatrixT,
+				typename RealT
+			>
+		controllable_decomposition(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+								   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B,
+								   RealT tol)
+	{
+		decompose(A, B, tol);
+	}
+
+
+	public: template <
+				typename AMatrixT,
+				typename BMatrixT
+			>
+		void decompose(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+					   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B)
+	{
+		namespace ublas = ::boost::numeric::ublas;
+		namespace ublasx = ::boost::numeric::ublasx;
+
+		//decompose(A, B, ublasx::num_rows(A)*ublas::norm_1(A)*::std::numeric_limits<value_type>::epsilon());, 
+		//decompose(A, B, ::std::max(ublasx::num_rows(A), ublasx::num_columns(A))*ublasx::eps(ublas::norm_2(A)));//FIXME: this is right
+		decompose(A, B, ::std::max(ublasx::num_rows(A), ublasx::num_columns(A))*ublasx::eps(value_type(ublas::norm_1(A))));//FIXME: this is wrong
+	}
+
+
+	public: template <
+				typename AMatrixT,
+				typename BMatrixT,
+				typename RealT
+			>
+		void decompose(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+					   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B,
+					   RealT tol)
+	{
+		namespace ublas = ::boost::numeric::ublas;
+		namespace ublasx = ::boost::numeric::ublasx;
+
+		typedef typename ublas::promote_traits<
+					typename ublas::promote_traits<
+						typename ublas::matrix_traits<AMatrixT>::size_type,
+						typename ublas::matrix_traits<BMatrixT>::size_type
+					>::promote_type,
+					size_type
+				>::promote_type work_size_type;
+		typedef typename ublas::promote_traits<
+					typename ublas::matrix_traits<AMatrixT>::value_type,
+					typename ublas::promote_traits<
+						typename ublas::matrix_traits<BMatrixT>::value_type,
+						typename ublas::promote_traits<
+							RealT,
+							value_type
+						>::promote_type
+					>::promote_type
+				>::promote_type work_value_type;
+
+		size_type A_nr(ublasx::num_rows(A));
+//		size_type A_nc(ublasx::num_columns(A));
+//		size_type B_nr(ublasx::num_rows(B));
+//		size_type B_nc(ublasx::num_columns(B));
+
+		// Initialization
+		k_ = ublas::zero_vector<size_type>(A_nr);
+		matrix_type PTj(ublas::identity_matrix<value_type>(A_nr, A_nr));
+		matrix_type Aj(A);
+		matrix_type Bj(B);
+//		size_type rojn1 = B_nc;
+		size_type deltaj = 0;
+//		size_type sigmaj(A_nr);
+
+		// Body loop
+		for (work_size_type j = 0; j < A_nr; ++j)
+		{
+			ublasx::svd_decomposition<value_type> svd;
+			svd.decompose(Bj);
+			matrix_type Uj(svd.U());
+			matrix_type Sj(svd.S());
+//			matrix_type Vj(svd.V());
+			size_type Sj_nr(ublasx::num_rows(Sj));
+//			size_type Sj_nc(ublasx::num_columns(Sj));
+
+			matrix_type P(ublasx::rot90(ublas::identity_matrix<value_type>(Sj_nr, Sj_nr), 1));
+			//ublasx::rot90(ublas::identity_matrix<value_type>(Sj_nr, Sj_nr));
+			//matrix_type P;
+			Uj = ublas::prod(Uj, P);
+
+			matrix_type BB(ublas::prod(ublas::trans(Uj), Bj));
+			size_type BB_nr(ublasx::num_rows(BB));
+//			size_type BB_nc(ublasx::num_columns(BB));
+
+			size_type roj(ublasx::rank(BB, tol));
+			size_type sigmaj(BB_nr-roj);
+			k_(j) = roj;
+			if (roj == 0 || sigmaj == 0)
+			{
+				break;
+			}
+			//matrix_type UAU = ublas::prod(ublas::prod(ublas::trans(Uj), Aj), Uj);
+			matrix_type UAU(ublas::prod(ublas::trans(Uj), Aj));
+			UAU = ublas::prod(UAU, Uj);
+			Aj = ublas::subrange(UAU, 0, sigmaj, 0, sigmaj);
+			Bj = ublas::subrange(UAU, 0, sigmaj, sigmaj, sigmaj+roj);
+			size_type Uj_nr(ublasx::num_rows(Uj));
+			size_type Uj_nc(ublasx::num_columns(Uj));
+			PTj = ublas::prod(
+					PTj,
+					ublasx::cat_columns(
+						ublasx::cat_rows(Uj, ublas::zero_matrix<value_type>(Uj_nr, deltaj)),
+						ublasx::cat_rows(ublas::zero_matrix<value_type>(deltaj, Uj_nc), ublas::identity_matrix<value_type>(deltaj, deltaj))
+					)
+				);
+			deltaj += roj;
+		}
+
+		// Finalization
+		T_ = ublas::trans(PTj);
+		//Ab_ = ublas::prod(ublas::prod(T_, A), PTj);
+		Ab_ = ublas::prod(T_, A);
+		Ab_ = ublas::prod(Ab_, PTj);
+		Bb_ = ublas::prod(T_, B);
+	}
+
+
+	/// Return the similarity transformation matrix T such that A_bar=TAT' and B_bar=TB.
+	public: matrix_type const& T() const
+	{
+		return T_;
+	}
+
+
+	/// Return the vector containing the number of controllable states at each iteration
+	public: vector_type const& k() const
+	{
+		return k_;
+	}
+
+
+	public: size_type num_controllable_states() const
+	{
+		return ::boost::numeric::ublasx::sum(k_);
+	}
+
+
+	/// Return the transformed A matrix.
+	public: matrix_type const& A_bar() const
+	{
+		return Ab_;
+	}
+
+
+	/// Return the transformed B matrix.
+	public: matrix_type const& B_bar() const
+	{
+		return Bb_;
+	}
+
+
+	public: template <typename MatrixT>
+		matrix_type C_bar(::boost::numeric::ublas::matrix_expression<MatrixT> const& C) const
+	{
+		namespace ublas = ::boost::numeric::ublas;
+
+		return ublas::prod(C, ublas::trans(T_));
+	}
+
+
+	private: matrix_type T_;
+	private: vector_type k_;
+	private: matrix_type Ab_;
+	private: matrix_type Bb_;
+};
+
+
+/*
+template <
+	typename AMatrixT,
+	typename BMatrixT
+>
+controllable_decomposition(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+						   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B)
+{
+	namespace ublas = ::boost::numeric::ublas;
+	namespace ublasx = ::boost::numeric::ublasx;
+
+	typedef typename ublas::promote_traits<
+				typename ublas::matrix_traits<AMatrixT>::value_type,
+				typename ublas::matrix_traits<BMatrixT>::value_type
+			>::promote_type value_type;
+
+	return controllable_decomposition(A, B, ublasx::num_rows(A)*ublas::norm_1(A)*::std::numeric_limits<value_type>::epsilon());, 
+}
+
+
+template <
+	typename AMatrixT,
+	typename BMatrixT,
+	typename RealT
+>
+controllable_decomposition(::boost::numeric::ublas::matrix_expression<AMatrixT> const& A,
+						   ::boost::numeric::ublas::matrix_expression<BMatrixT> const& B,
+						   ::boost::numeric::ublas::matrix_expression<CMatrixT> const& C,
+						   RealT tol)
+{
+	namespace ublas = ::boost::numeric::ublas;
+	namespace ublasx = ::boost::numeric::ublasx;
+
+	typedef typename ublas::promote_traits<
+				typename ublas::matrix_traits<AMatrixT>::size_type,
+				typename ublas::matrix_traits<BMatrixT>::size_type
+			>::promote_type size_type;
+	typedef typename ublas::promote_traits<
+				typename ublas::matrix_traits<AMatrixT>::value_type,
+				typename ublas::promote_traits<
+					typename ublas::matrix_traits<BMatrixT>::value_type,
+					RealT
+				>::promote_type
+			>::promote_type value_type;
+	typedef ublas::matrix<value_type> work_matrix_type;
+	typedef ublas::vector<value_type> work_vector_type;
+
+	size_type A_nr(ublasx::num_rows(A));
+	size_type A_nc(ublasx::num_columns(A));
+	size_type B_nr(ublasx::num_rows(B));
+//	size_type B_nc(ublasx::num_columns(B));
+
+	// Initialization
+	work_matrix_type PTj(ublas::identity_matrix<value_type>(A_nr, A_nr));
+	work_matrix_type Aj(A);
+	work_matrix_type Bj(B);
+//	size_type rojn1 = B_nc;
+	size_type deltaj = 0;
+//	size_type sigmaj(A_nr);
+	work_vector_type k(ublas::zero_vector<value_type>(A_nr));
+
+	// Body loop
+	for (size_type j = 0; j < A_nr; ++j)
+	{
+		ublasx::svd_decomposition<value_type> svd;
+		svd.decompose(Bj);
+		work_matrix_type Uj(svd.U());
+		work_matrix_type Sj(svd.S());
+		work_matrix_type Vj(svd.V());
+		size_type S_nr(ublasx::num_rows(S));
+		size_type S_nc(ublasx::num_columns(S));
+
+		work_matrix_type P(ublasx::rot90(ublas::identity_matrix<value_type>(S_nr, S_nr)));
+		Uj = ublas::prod(Uj, P);
+
+		work_matrix_type BB(ublas::prod(ublas::trans(Uj), Bj));
+		BB_nr = ublasx::num_rows(BB);
+		BB_nc = ublasx::num_columns(BB);
+
+		size_type roj(ublasx::rank(BB, tol));
+		size_type sigmaj(BB_nr-roj);
+		k(jj) = roj;
+		if (roj == 0 || sigmaj == 0)
+		{
+			break;
+		}
+		abxy = ublas::prod(ublas::prod(ublas::trans(Uj), Aj), Uj);
+		Aj = ublas::subrange(abxy, 0, sigmaj, 0, sigmaj);
+		Bj = ublas::subrange(abxy, 0, sigmaj, sigmaj, sigmaj+roj);
+		size_type Uj_nr(ublasx::num_rows(Uj));
+		size_type Uj_nc(ublasx::num_columns(Uj));
+		PTj = ublas::prod(
+				PTj,
+				ublasx::cat_columns(
+					ublasx::cat_rows(U, ublas::zero_matrix<value_type>(Uj_nr, deltaj)),
+					ublasx::cat_rows(ublas::zero_matrix<value_type>(deltaj, Uj_nc, ublas::identity_matrix<value_type>(deltaj, deltaj)))
+				)
+			);
+		deltaj += roj;
+	}
+
+	// Finalization
+	T = ublas::trans(PTj);
+	//A_bar = ublas::prod(ublas::prod(T, a), ublas::trans(T));
+	A_bar = ublas::prod(ublas::prod(t, a), PTj);
+	B_bar = ublas::prod(T, b);
+	//cbar = ublas::prod(c, ublas::trans(T));
+	C_bar = ublas::prod(c, PTj);
+}
+*/
 
 }} // Namespace dcs::control
 
